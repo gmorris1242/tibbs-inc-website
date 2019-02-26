43652b96cd8d6b08965759c0a5fa571ccd1b37a1
George Morris <gmorris1242@gmail.com>
2019-02-26 16:59:19 -0500

    More work on homepage

---
+++ app/assets/javascripts/slider.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+  $('.slider').slick({
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: false,
+    buttons: true,
+    autoplay: true,
+    autoplaySpeed: 1000,
+  });
+});
