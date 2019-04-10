ba9a990026967912a2059d1a96f0974581376839
George Morris <gmorris1242@gmail.com>
2019-04-10 17:03:49 -0400

    Start case studies

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,5 @@ Rails.application.routes.draw do
   get 'our-work' => 'our_work#index'
   get 'case-studies' => 'pages#case_studies'
   get 'case-studies/mike-the-barber' => 'pages#mike_the_barber'
+  get 'case-studies/4x4_day' => 'pages#4x4_day'
 end
