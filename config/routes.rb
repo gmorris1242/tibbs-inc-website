bf5e8baf863e5e304b7efbef740c298b461bfee8
George Morris <gmorris1242@gmail.com>
2019-04-09 20:35:58 -0400

    Add new videos and posters

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,5 @@ Rails.application.routes.draw do
 
   get 'our-work' => 'our_work#index'
   get 'case-studies' => 'pages#case_studies'
+  get 'case-studies/mike-the-barber' => 'pages#mike_the_barber'
 end
